d2fd30e7d491e727f908d08f9b48bee88fb66bb6
s.tihomirov <tichomirovsergey@gmail.com>
2013-12-19 14:56:26 +0400

    I improved colors of reporting and highlighting borders.

--- primitive/src/main/java/org/primitive/logging/eLogColors.java
+++ primitive/src/main/java/org/primitive/logging/eLogColors.java
@@ -3,9 +3,9 @@ package org.primitive.logging;
 import java.awt.Color;
 
 public enum eLogColors {
-	CORRECTSTATECOLOR(new Color(0, 139, 0), "Green4"),
-	WARNSTATECOLOR(new Color(255, 69, 0),"OrangeRed"),
-	DEBUGCOLOR(new Color(255, 250, 250), "Snow"), SEVERESTATECOLOR(new Color(139, 0, 0 ),"DarkRed");
+	CORRECTSTATECOLOR(new Color(0, 255, 0 ), "Green"),
+	WARNSTATECOLOR(new Color(255, 255, 0 ),"Yellow"),
+	DEBUGCOLOR(new Color(255, 250, 250), "Snow"), SEVERESTATECOLOR(new Color(255, 0, 0 ),"Red");
 	
 	private Color stateColor;
 	private String htmlColorDescription;
