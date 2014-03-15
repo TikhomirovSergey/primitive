606379e70f1150c4f33413d6af4b32b7dd8e055a
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2014-03-15 17:35:57 +0400

    The example of the test on an Android application. I didn't verify this. If there are mistakes, I will fix it soon.

---
+++ primitive/src/test/java/apps/testexample/TestExamples.java
@@ -0,0 +1,33 @@
+package apps.testexample;
+
+import org.primitive.configuration.Configuration;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import apps.app.MobileApplicationClient;
+import apps.app.android.ContactManagerForm;
+
+public class TestExamples {
+	
+	@Test
+	@Parameters(value = { "config" })
+	public void testOnAndroidContactManagerApp(
+			@Optional("androidApp.json") String config) {
+		Configuration configuration = Configuration
+				.get("src/test/resources/configs/desctop/" + config);
+		MobileApplicationClient client = MobileApplicationClient
+				.getNew(configuration);
+
+		try {
+			ContactManagerForm contactManager = client
+					.getPart(ContactManagerForm.class);
+			contactManager.addContactClick();
+			contactManager.setName("Some Name");
+			contactManager.setEmail("Some@example.com");
+		} finally {
+			client.quit();
+		}
+
+	}
+}
