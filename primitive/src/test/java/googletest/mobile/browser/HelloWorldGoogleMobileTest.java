c8e320b44cd0147a95b53811121647a7b1bb7541
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2014-04-20 13:15:40 +0400

    #5. This one is 75% worked out. I have to prepare an example how to test app (not a web site) on iOS.

--- primitive/src/test/java/googletest/mobile/browser/HelloWorldGoogleMobileTest.java
+++ primitive/src/test/java/googletest/mobile/browser/HelloWorldGoogleMobileTest.java
@@ -58,6 +58,25 @@ public class HelloWorldGoogleMobileTest {
 		launchTest(mobileOS, config);
 	}
 	
+	/**
+	 * * JSON setting see here: /primitive/src/test/resources/configs/mobile/android/browser/ios_safari.json
+	 *Appium settings: - IP Address = 0.0.0.0
+	 * 				   - Port = 4723
+	 * 				   - iOS - 	is switched on
+	 * 				   - Force Device = yes - iPhone Retina (4-inch)
+	 * 				   - Use mobile Safari = yes
+	 * Environment:
+	 * 				   - Virtual machine that is made by VMWare.Net bridge
+	 * 				   - Mac OS 10.8.5 or newer as a guest OS.
+	 * 				   - xCode 4.6.3 or newer is installed
+	 * 				   - iPhone Simulator for iOS 6.1 is there
+	 * Test is launched on Win 8 but it is run on Mac OS virtual machine.
+	 * There are many problems with xCode 5.x whose descriptions can be googled. I think it is not Appium friendly for now.
+	 * Also, issues can be there because I use virtual machine instead of real Mac. It should be checked out
+	 * on a real macintosh. 
+	 * ...so I use xCode 4.6.3 for this one. 
+	 * I use Appium as a remote server for WebDriver there.    
+	 */
 	@Test(description = "This is just a test of basic functionality on IOS Safari using Appium")
 	@Parameters(value = {"mobileOS",  "config" })
 	public void typeHelloWorldAndOpenTheFirstLinkOnIOSSafari(
@@ -66,8 +85,26 @@ public class HelloWorldGoogleMobileTest {
 		launchTest(mobileOS, config);
 	}
 
+	/**
+	 * * JSON setting see here: /primitive/src/test/resources/configs/mobile/android/browser/ios_safari_localMac.json
+	 *Appium settings: - IP Address = 0.0.0.0
+	 * 				   - Port = 4723
+	 * 				   - iOS - 	is switched on
+	 * 				   - Force Device = yes - iPhone Retina (4-inch)
+	 * 				   - Use mobile Safari = yes
+	 * Environment:
+	 * 				   - Virtual machine that is made by VMWare.Net bridge
+	 * 				   - Mac OS 10.8.5 or newer as a guest OS.
+	 * 				   - xCode 4.6.3 or newer is installed
+	 * 				   - iPhone Simulator for iOS 6.1 is there
+	 * Test is launched and is run on Mac OS virtual machine (local starting).
+	 * There are many problems with xCode 5.x whose descriptions can be googled. I think it is not Appium friendly for now.
+	 * Also, issues can be there because I use virtual machine instead of real Mac. It should be checked out
+	 * on a real macintosh. 
+	 * ...so I use xCode 4.6.3 for this example.    
+	 */
 	@Test(description = "This is just a test of basic functionality on IOS Safari using Appium. Test is run on a local Mac. "
-			+ "As for me it is a virtual machine with Mac OS X Mavericks as the guest.")
+			+ "As for me it is a virtual machine with Mac OS X 10.8.5 as the guest.")
 	@Parameters(value = {"ios_safari_localMac.json",  "config" })
 	public void typeHelloWorldAndOpenTheFirstLinkOnIOSSafari_OnALocalMac(
 			@Optional("ios") String mobileOS,
